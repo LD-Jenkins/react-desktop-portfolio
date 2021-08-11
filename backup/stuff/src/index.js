3e86db0e07ac0d50b9000d504976898bec358d28
Leighton Jenkins <leightonjenkins84@gmail.com>
2021-08-11 00:50:39 -0500

    readded last changes

--- backup/stuff/src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import Computer from './Computer';
-
-ReactDOM.render(
-  <Computer />,
-  document.getElementById('root')
-);
